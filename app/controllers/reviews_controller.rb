28c7457b9d9e9ad6bd805d5e92d19c5ac839137f
Chris Westerman <chris.westerman8@gmail.com>
2020-05-13 15:15:49 +0100

    added validations for new reviews and restaurants

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -20,14 +20,25 @@ class ReviewsController < ApplicationController
   end
 
   def create
+    @review = Review.new(review_params)
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review.restaurant = @restaurant
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
   end
 
   def destroy
+    @review.destroy
+    redirect_to restaurant_path(@review.restaurant_id)
   end
 
   private
 
   def review_params
+    params.require(:review).permit(:rating, :content)
   end
 
   def set_review
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,10 @@
 class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
 
+  categories = ["chinese", "italian", "japanese", "french", "belgian"]
+
   validates :name, presence: true, uniqueness: true
   validates :address, presence: true
   validates :phone_number, presence: true, uniqueness: true
-  validates :category, presence: true
+  validates :category, presence: true, inclusion: { in: categories }
 end
